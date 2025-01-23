8fd7e5ff6246df28d9351fd709e93c3b47cd0f24
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-01-23 12:29:27 +0100

    Add export to unused variables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const animals = [
+export const animals = [
   { id: 1, firstName: 'Lucia', type: 'Dog', accessory: 'House' },
   { id: 2, firstName: 'Macco', type: 'Fish', accessory: 'Car' },
   { id: 3, firstName: 'Jaspa', type: 'Elephant', accessory: 'Bike' },
@@ -11,4 +11,6 @@ function greet(name) {
 
 greet();
 
-const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+export const response = await fetch(
+  'https://jsonplaceholder.typicode.com/todos/1',
+);
