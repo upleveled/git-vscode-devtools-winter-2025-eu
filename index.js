40bc7923ed14dc29f80049956e6a1ab7f7517bc3
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-01-23 12:28:11 +0100

    Add animals array

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const animals = [
+  { id: 1, firstName: 'Lucia', type: 'Dog', accessory: 'House' },
+  { id: 2, firstName: 'Macco', type: 'Fish', accessory: 'Car' },
+  { id: 3, firstName: 'Jaspa', type: 'Elephant', accessory: 'Bike' },
+  { id: 4, firstName: 'Jacco', type: 'Duck', accessory: 'Truck' },
+  { id: 5, firstName: 'Malli', type: 'Cat', accessory: 'Lorry' },
+];
 function greet(name) {
   console.log(`Hello ${name}`);
 }
 
 greet();
+
+const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
